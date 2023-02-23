afdca50042fc91c158fe1750575077f5048a6537
feefayfay <feefayfay@localhost>
2023-02-23 13:54:08 -0600

    fin

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,26 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shapeType = in.next();
+		int color = in.nextInt();
+		int color2 = in.nextInt();
+		int color3 = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double param = in.nextDouble();
+		double param2 = in.nextDouble();
+		double param3 = in.nextDouble();
+		double param4 = in.nextDouble();		
+		StdDraw.setPenColor(color, color2, color3);
+		if (shapeType.equals("ellipse"))
+			{
+			if (filled)
+			{
+				StdDraw.filledEllipse(param, param2, param3, param4);
+			}
+			else
+			{
+				StdDraw.ellipse(param, param2, param3, param4);
+			}
+			}
 	}
 }
